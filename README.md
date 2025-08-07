911be8400e86ae3eb076ced7e68b825f2e48fdc2
eafiseemn <eafiseemn@gmail.com>
2025-08-07 17:02:25 +0900

    Chapter04. Form Binding (v-bind & v-on, v-model)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Programmers FE 부트캠프
+
+### Vue.js 실습
+
+Vue.js 공식 [튜토리얼 페이지](https://ko.vuejs.org/tutorial/#step-1) 에서 제공하는 실습 예제들을
+하나씩 만들어 보면서, Vue에 대해 공부합니다. 
---
+++ vue-practice/src/components/Chapter04.vue
@@ -0,0 +1,42 @@
+<!-- ----------------------------------------------------------------------- -->
+<!--                              Form Binding                               -->
+<!-- ----------------------------------------------------------------------- -->
+
+<!-- 
+v-bind 와 v-on을 함께 사용하면 폼 입력 요소에 양방향 데이터 바인딩이 가능합니다.
+vue는 브라우저의 Event를 그대로 사용합니다.
+
+v-model은 input값이 바인딩 된 상태와 자동으로 동기화 됩니다. (이벤트 핸들러가 필요하지 않습니다.)
+
+v-model은 텍스트 뿐만 아니라 체크박스, 라디오, 드롭다운 등 다양한 인터렉션 태그들에 사용 가능합니다.
+-->
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const text = ref('');
+const modelText = ref('');
+
+const onChangeInput = (e: Event) => {
+  // console.log('changed!');
+  const target = e.target as HTMLInputElement;
+  text.value = target.value;
+}
+
+</script>
+
+<template>
+  <h2>Ref & EventHandler </h2>
+  <input type="text" @input="onChangeInput" />
+  <p>입력 값 : {{ text }}</p>
+
+  <hr />
+
+  <h2>V-Model</h2>
+  <input type="text" v-model="modelText" />
+  <p>입력 값 : {{ modelText }} </p>
+</template>
+
+<style scoped>
+
+</style>
